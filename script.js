203ff0608f52b4c37d9f2316d2fd4241b0dfc61e
eldar3mk <2023stevemc@gmail.com>
2026-02-16 22:05:18 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+var moveSound = new Audio("https://cdn.pixabay.com/download/audio/2022/03/15/audio_9a6e7d.mp3");
+var captureSound = new Audio("https://cdn.pixabay.com/download/audio/2022/03/15/audio_8b5ed7.mp3");
+var checkSound = new Audio("https://cdn.pixabay.com/download/audio/2022/03/15/audio_2a2e6f.mp3");
